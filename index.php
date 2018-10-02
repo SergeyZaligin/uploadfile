6694a7e771f81f144f5437ebc4eb17831e65d853
Konstantin Schmidt <31983699+Assimilationstheorie@users.noreply.github.com>
2018-10-02 11:57:28 +0200

    add placeholder and co

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 </head>
 <body>
 	<form name="form" action="create.php" enctype="multipart/form-data" method="post">
-		<input type="file" name="image" title="Выберите файл"/>
-		<input type="submit" name="submit" value="send" /> 
+		<input type="file" name="image" title="Выберите файл" placeholder="Выберите файл" required/>
+		<input type="submit" name="submit" value="Send" /> 
 	</form>
 </body>
 </html>
